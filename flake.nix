ca3a585810c48e6a5f29948784ce1b9799d62d5b
LarsZauberer <wasser.ian@gmail.com>
2025-07-24 10:45:39 +0200

    Basic flake for BreezeX-Black hyprcursor theme

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Flake for the BreezeX-Hyprcursor Theme";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = inputs: let
+    pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
+  in {
+    packages.x86_64-linux.black = pkgs.stdenv.mkDerivation {
+      pname = "BreezeX-Hyprcursor-Black";
+      version = "2.0.1";
+
+      src = ./black;
+
+      buildPhase = "";
+      installPhase = ''
+        mkdir -p $out/BreezeX-Black
+        mv ./* $out/BreezeX-Black
+      '';
+    };
+  };
+}
